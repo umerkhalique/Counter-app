0545aa88eb2b373dd07f4e24873450b330017800
umer khalique <omerkhalique13@gmail.com>
2022-02-15 18:56:08 +0500

    added hide subtract button on zero functionality

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
+window.onload = function() {
+    element.style.display = 'none';
+    document.getElementById('counter').classList.add("btn-hide");
+  };
 var counter = 0;
+let element = document.getElementById("btn-danger");
 
 function add() {
     counter++;
     document.getElementById('counter').innerHTML = counter;
+    if(counter > 0){
+        element.style.display = 'flex';
+    }
+
 };
 function subtract() {
+
     counter--;
     document.getElementById('counter').innerHTML = counter;
+    if(counter === 0){
+        
+        element.style.display = 'none';
+        document.getElementById('counter').classList.add("btn-hide");
+    }
+    
 }
 function reset(){
     counter = 0;
     document.getElementById('counter').innerHTML = counter;
+    if(counter == 0){
+        
+        element.style.display = 'none';
+        document.getElementById('counter').classList.add("btn-hide");
+    }
+    
+
 }
